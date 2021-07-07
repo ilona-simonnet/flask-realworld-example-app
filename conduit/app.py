a18d7b859cbb27b25d4d8cb70e8749b0796a53bb
ilona-simonnet <86947510+ilona-simonnet@users.noreply.github.com>
2021-07-07 14:37:51 +0200

    Update app.py
    modif conduit/app.py (ajout param SSL)

--- conduit/app.py
+++ conduit/app.py
@@ -3,6 +3,15 @@
 from flask import Flask
 from conduit.extensions import bcrypt, cache, db, migrate, jwt, cors
 
+
+
+from OpenSSL import SSL
+context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+context.use_privatekey_file('ca-key.pem')
+context.use_certificate_file('server-cert.pem')   
+
+
+
 from conduit import commands, user, profile, articles
 from conduit.settings import ProdConfig
 from conduit.exceptions import InvalidUsage
